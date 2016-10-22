656257f5677b3a1edafb986df601a3469f908575
Jason Harrelson <jason@lookforwardenterprises.com>
2016-10-21 23:30:09 -0500

    Update ecto dependency to ~> 2.0.

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule PhilColumns.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ecto, "~> 1.0"},
+      {:ecto, "~> 2.0"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:inflex, "~> 1.6"}
     ]
