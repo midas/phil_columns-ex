e6929d9c9497dafaca4f189d6f2b70c1fb564539
Jason Harrelson <jason@lookforwardenterprises.com>
2017-02-23 22:03:13 -0600

    Bump version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhilColumns.Mixfile do
 
   def project do
     [app: :phil_columns,
-     version: "0.9.0",
+     version: "1.0.0",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
