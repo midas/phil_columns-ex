f4a20b9c692beb9c5cf855a8c6d7037b1b453c86
Donald A. Lobo <donald.lobo@gmail.com>
2020-07-23 10:25:13 -0700

    upgrade inflex dependency

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule PhilColumns.Mixfile do
     [
       {:ecto_sql, "~> 3.0"},
       {:ex_doc, "~> 0.20", only: :dev},
-      {:inflex, "~> 1.8"}
+      {:inflex, "~> 2.0"}
     ]
   end
 end
